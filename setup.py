d28f1dce28642724c810e993dc3770d8abf2f0ba
Henrik <sigstad@g.harvard.edu>
2019-07-31 10:31:10 +0200

    Updating info, adding data

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="diarios",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Guilherme Lambais and Henrik Sigstad",
+    author_email="h.sigstad@gmail.com",
+    description="Extract information from Brazilian official diaries",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/hsigstad/diarios",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
